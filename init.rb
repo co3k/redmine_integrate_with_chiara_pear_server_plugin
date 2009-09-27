53253c5263d034f138c2b5f721858c89f5777dec
Kousuke Ebihara <ebihara@tejimaya.com>
2009-09-27 17:30:25 +0900

    started development of redmine_integrate_with_chiara_pear_server_plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_integrate_with_chiara_pear_server_plugin do
+  name 'Redmine Integrate With Chiara Pear Server Plugin plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
